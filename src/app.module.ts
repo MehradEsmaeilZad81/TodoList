5dd7d7c0036021f9e496f70101af87442f5c1831
mehradEsmaeilZad <mehrad.es81@gmail.com>
2025-08-29 18:42:41 +0330

    Add Prisma integration with PostgreSQL support and update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { envValidationSchema } from './config/validation';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AppService } from './app.service';
       validationSchema: envValidationSchema,
       expandVariables: true,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
